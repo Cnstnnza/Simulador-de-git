98d4eb0f31418663c8d49dc62a7bd92ec303d3aa
Constanza <conaraya@umag.cl>
2025-09-04 20:55:00 -0300

    avance

--- main.c
+++ main.c
@@ -1,4 +1,31 @@
 // simulador git
 #include <stdio.h>
-aaaaaa
-intddddddddddddd
+#include <string.h>
+#include <sys/stat.h>
+
+void init_repo(char *nombre);
+void delete_repo(char *nombre);
+
+int main(){
+    char carpeta[40];
+
+    return 0;
+}
+
+void init_repo(char *nombre)
+{
+    if (mkdir(nombre, 0700) == 0) {
+        printf("Repositorio '%s' creado correctamente.\n", nombre);
+    } else {
+        perror("Error al crear el repositorio");
+    }
+}
+
+void delete_repo(char *nombre)
+{
+    if (rmdir(nombre) == 0) {
+        printf("Repositorio '%s' eliminado correctamente.\n", nombre);
+    } else {
+        perror("Error al eliminar el repositorio");
+    }
+}
