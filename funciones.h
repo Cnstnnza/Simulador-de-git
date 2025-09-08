6bf0b08d8697857674f10b8804114c7f0cfb32e5
Constanza <conaraya@umag.cl>
2025-09-07 21:20:18 -0300

    Implementacion de algunas funciones

---
+++ funciones.h
@@ -0,0 +1,9 @@
+#ifndef FUNCIONES_H   // protección para no incluirlo dos veces
+#define FUNCIONES_H
+
+void help();
+void init_repo(char *nombre);
+void delete_repo(char *nombre);
+
+
+#endif
--- main.c
+++ main.c
@@ -2,13 +2,58 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
+#include "funciones.h"
 
-void init_repo(char *nombre);
-void delete_repo(char *nombre);
 
 int main(){
     char carpeta[40];
+    char comando[10];
 
+    printf("Bienvenido a Git Simulator\n");
+    printf("Si necesitas ayuda, escribe 'help'\n");
+
+    while(1)
+    {   
+        printf("git-simulator> ");
+        fgets(comando, sizeof(comando), stdin);
+        comando[strcspn(comando, "\n")] = 0;
+
+        if(strcmp(comando, "exit") == 0)
+        {
+            printf("Saliendo del simulador de Git. ¡Hasta luego!\n");
+            break;
+        }
+        else if(strcmp(comando, "help") == 0)
+        {
+            help();
+        }
+        else if(strcmp(comando, "git init") == 0)
+        {
+            printf("Ingrese el nombre del repositorio a crear: ");
+            scanf("%s", carpeta);
+            init_repo(carpeta);
+        }
+        else if(strcmp(comando, "git delete") == 0)
+        {
+            printf("Ingrese el nombre del repositorio a eliminar: ");
+            scanf("%s", carpeta);
+            printf("¿Está seguro de que desea eliminar el repositorio '%s'? (S/N): ", carpeta);
+            fgets(comando, sizeof(comando), stdin);
+            comando[strcspn(comando, "\n")] = 0;
+            if(strcmp(comando, "S") == 0)
+            {
+                delete_repo(carpeta);
+            }
+            else
+            {
+                printf("No se eliminó el repositorio '%s'.\n", carpeta);
+            }
+        }
+        else 
+        {
+            printf("Comando no reconocido. Escriba 'help' para ver los comandos disponibles.\n");
+        }
+    }
     return 0;
 }
 
@@ -28,4 +73,13 @@ void delete_repo(char *nombre)
     } else {
         perror("Error al eliminar el repositorio");
     }
+}
+
+void help(){
+    printf("COMANDOS DISPONIBLES\n");
+    printf("git init\t\t\tInicializa un repositorio\n");
+    printf("git pull\t\t\tConsigue los cambios del repositorio remoto\n");
+    printf("git push\t\t\tEnvía los cambios al repositorio remoto\n");
+    printf("git add\t\t\tAñade un archivo al repositorio\n");
+    printf("git commit\t\t\tGuarda los cambios en el repositorio\n");
 }
